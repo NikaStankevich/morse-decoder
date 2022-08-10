17ff3658473f3dd2ce336e49e739c43be12bf3ed
Nika Stankevich <kladnickaya@gmail.com>
2022-08-11 02:28:22 +0300

    feat: implement morse deceoder

--- src/index.js
+++ src/index.js
@@ -37,8 +37,36 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const BINARY_TO_MORSE = {
+    '00': '',
+    '10': '.',
+    '11': '-'
+}
+
 function decode(expr) {
-    // write your solution here
+    function decodeSymbol(symbol) {
+        let morseStr = '';
+        let symbolArr = symbol.match(/.{1,2}/g);
+
+        symbolArr.forEach(el => {
+            morseStr = morseStr + BINARY_TO_MORSE[el];
+        });
+
+        return morseStr;
+    }
+
+    let messageArr = expr.match(/.{1,10}/g);
+    let messageStr = '';
+
+    messageArr.forEach(el => {
+        if (el === '**********') {
+            messageStr = messageStr + ' ';
+        } else {
+            messageStr = messageStr + MORSE_TABLE[decodeSymbol(el)];
+        }
+    });
+
+    return messageStr;
 }
 
 module.exports = {
